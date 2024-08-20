18585cdf90872cdd57d6cdade35b13fdd997d33f
Touch Sangrotha <49270177+YungTax@users.noreply.github.com>
2024-08-20 15:44:37 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -23,16 +23,16 @@ define( 'WP_CACHE', true );
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'sporthub' );
+define( 'DB_NAME', 'u985379591_Z2e4g' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u985379591_ICtbM' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', '3jTlc1PYO4' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '127.0.0.1' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
